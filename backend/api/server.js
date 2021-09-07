9aaf186b30188b5ee766d5c3f0d20e21d004c2b9
Chris Lau <chrislyc.lau@gmail.com>
2021-09-07 10:54:32 -0700

    movement of a single user is working

--- backend/api/server.js
+++ backend/api/server.js
@@ -12,9 +12,40 @@ require("../interfaces");
 server.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+var users = {
+// key : user_id
+// value :{
+//    user_id
+//    username
+//}
+};
 io.on('connection', function (socket) {
+    if (!socket.handshake.query || !socket.handshake.query.username) {
+        return;
+    }
+    var username = socket.handshake.query.username;
+    io.to(socket.id).emit("initial self position", {
+        user_id: socket.id,
+        username: username,
+        room_id: "",
+        x: 50,
+        y: 50
+    });
+    socket.on("position", function (position) {
+        // will implement different rooms later. for now, just emit the position to every socket
+        io.volatile.emit("position", position);
+    });
+    socket.on("join user", function (socket_id) {
+        // join the channels of other users
+        socket.join(socket_id);
+    });
+    socket.on("leave user", function (socket_id) {
+        // leave the channels of ther users
+        socket.leave(socket_id);
+    });
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        // emit to the channel belong to the socket. Identifiable through id
+        io.to(socket.id).emit(msg);
     });
 });
 module.exports = http;
--- backend/api/server.ts
+++ backend/api/server.ts
@@ -14,11 +14,26 @@ server.get('/', (req:any, res:any) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.use((socket:any,next:any)=>{
-//   //middleware for every incoming sockets
-// })
+const users = {
+  // key : user_id
+  // value :{
+  //    user_id
+  //    username
+  //}
+}
 
 io.on('connection', (socket:any) => {
+  if(!socket.handshake.query || !socket.handshake.query.username){
+    return;
+  }
+  const {username} = socket.handshake.query;
+  io.to(socket.id).emit("initial self position",{
+    user_id:socket.id,
+    username,
+    room_id:"",
+    x:50,
+    y:50
+  } as Position);
   socket.on("position",(position:Position)=>{
     // will implement different rooms later. for now, just emit the position to every socket
     io.volatile.emit("position",position);
@@ -36,57 +51,5 @@ io.on('connection', (socket:any) => {
     io.to(socket.id).emit(msg);
   });
 });
-
-
-
-// const config = {
-//   name:"sessionId",
-//   secret: "keep it secret, keep it safe",
-//   cookie:{
-//     maxAge: 1000 * 60 * 60,
-//     secure:false,
-//     httpOnly: true
-//   },
-//   resave:false,
-//   saveUnitialized:false,
-//   store: new KnexSessionStore({
-//     knex:require("../database/db-config.js"),
-//     tablename:"sessions",
-//     sidfieldname:"sid",
-//     createTable:true,
-//     clearInterval:1000 * 60 * 60
-//   })
-// }
-// server.use(express.json());
-// server.use("/api/auth",authRouter);
-// server.use("/api/rooms",roomsRouter);
-// server.use("/api/users",usersRouter);
-// server.use(session(config));
-
-
-//this should be in the frontend
-// const isInProximity=(pos1:Position,pos2:Position,threshold:number):boolean=>{
-//   const distance = Math.sqrt(Math.pow(pos1.x-pos2.x,2)+Math.pow(pos1.y-pos2.y,2));
-//   return distance <= threshold;
-// }
-
-// io.on("connection",(socket:any)=>{
-//   socket.on("position change",(position:Position)=>{
-//     socket.volatile.emit("position change",position);
-//   });
-//   socket.on("video call request",(caller_id:number,callee_id:number)=>{
-//     socket.to(callee_id).emit("video call",caller_id);
-//   });
-//   socket.on("peer connection ends",(socket_id1:number,socket_id2:number)=>{
-
-//   })
-//   socket.on("disconnect",(position:Position)=>{
-//     socket.emit("remove position",position);
-//   });
-//   socket.on("create-offer",(data:any)=>{
-//     const {sdp,receipient, sender} = data;
-
-//   })
-// })
 
 export = http;
